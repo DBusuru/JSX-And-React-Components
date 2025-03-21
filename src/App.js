d3e9689ff9533d239ea28a3cc70a0c3a51e0770c
DBusuru <davidbusuru1@gmail.com>
2025-03-21 16:36:19 +0300

    JSX-And-React-Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Card, Container } from "react-bootstrap";
+import Name from "./Name";
+import Price from "./Price";
+import Description from "./Description";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const firstName = ""; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="d-flex flex-column align-items-center mt-5">
+      <Card style={{ width: "18rem", textAlign: "center" }} className="p-3 shadow-lg">
+        {/* Fixed: Use img instead of Image component */}
+        <img src="/Assets/image.jpg" alt="Product" className="card-img-top" />
+        <Card.Body>
+          <Name />
+          <Price />
+          <Description />
+        </Card.Body>
+      </Card>
+
+      <h3 className="mt-3">Hello, {firstName ? firstName : "there"}!</h3>
+
+      {firstName && (
+        <img
+          src="/Assets/image.jpg" 
+          alt="User avatar"
+          className="mt-2 rounded-circle"
+          width="100"
+        />
+      )}
+    </Container>
   );
 }
 
